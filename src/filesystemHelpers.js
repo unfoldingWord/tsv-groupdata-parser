9c7aacbe08cb65e8cbba490344f93f61b9857a81
mannycolon <colonmanuel7@gmail.com>
2019-02-18 16:17:27 -0500

    promsified formatAndSaveGroupData()

--- src/filesystemHelpers.js
+++ src/filesystemHelpers.js
@@ -4,20 +4,28 @@ import path from 'path-extra'
 /**
  * Formats and saves the groupData into the 5 tN categories in the desired
  * filesystem structure (lexical, figures, cultural, morphological and other)
- * @param {object} categorizedGroupData groupData categorized in 5 categories: 
+ * which include all the json files for the catgory based on the tA articles
+ * @param {object} categorizedGroupData groupData categorized in 5 categories:
  * lexical, figures, cultural, morphological and other.
  * @param {string} rootDestinationPath root directory where the files will be saved.
  * @param {string} bookId book id.
  */
 export const formatAndSaveGroupData = (categorizedGroupData, rootDestinationPath, bookId) => {
-  Object.keys(categorizedGroupData).forEach(categoryName => {
-    const categoryData = categorizedGroupData[categoryName];
-    Object.keys(categoryData).forEach(groupId => {
-      const filename = groupId + '.json'
-      const groupData = categoryData[groupId]
-      const savePath = path.join(rootDestinationPath, categoryName, 'groups', bookId, filename)
+  return new Promise((resolve, reject) => {
+    try {
+      Object.keys(categorizedGroupData).forEach(categoryName => {
+        const categoryData = categorizedGroupData[categoryName];
+        Object.keys(categoryData).forEach(groupId => {
+          const filename = groupId + '.json'
+          const groupData = categoryData[groupId]
+          const savePath = path.join(rootDestinationPath, categoryName, 'groups', bookId, filename)
 
-      fs.outputJsonSync(savePath, groupData)
-    })
+          fs.outputJsonSync(savePath, groupData)
+        })
+      })
+      resolve()
+    } catch (error) {
+      reject(error)
+    }
   })
 }
