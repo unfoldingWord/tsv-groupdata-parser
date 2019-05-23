6a3b101dcd30be8042efb685bdee42d0560019c1
mannycolon <colonmanuel7@gmail.com>
2019-05-23 16:07:05 -0400

    Removed console logs

--- src/wordOccurrenceHelpers.js
+++ src/wordOccurrenceHelpers.js
@@ -27,7 +27,6 @@ export function getQuoteOmittedString(quote, verseString) {
 
     // if index plus 1 is odd & is not the last item in the array
     if (indexPlusOneIsOdd(index) && index < (quoteChunks.length - 1) && index !== 2) {
-      console.log('1- index < (quoteChunks.length - 1)')
       let nextQuoteChunk = quoteChunks[index + 1]
       if (!verseString.includes(nextQuoteChunk)) nextQuoteChunk = nextQuoteChunk.trim()
       const nextChunkIndex = verseString.indexOf(nextQuoteChunk)
@@ -52,7 +51,6 @@ export function getQuoteOmittedString(quote, verseString) {
       }
       missingWordsIndices.push(quoteChunkSubStrIndex + (index === 0 ? quoteChunk.length : 0))
     } else if ((index === (quoteChunks.length - 1) || index >= 2) && quoteChunks.length >= 3) {// if is last quote chunk
-      console.log('2- last chunk')
       const lastMissingWordEndingIndex = verseString.indexOf(quoteChunk);
       const sliced = verseString.slice(lastMissingWordEndingIndex)
       const stringPrecedingLastChunk = verseString.replace(sliced, '')
@@ -61,7 +59,6 @@ export function getQuoteOmittedString(quote, verseString) {
 
       missingWordsIndices.push(startIndex + (index === 0 ? quoteChunk.length : 0))
 
-      // console.log(index, verseString.includes(quoteChunk), 'included')
       if (!verseString.includes(quoteChunk)) {
         quoteChunk = quoteChunk.trim()
         quoteChunks[index] = quoteChunk
@@ -69,7 +66,6 @@ export function getQuoteOmittedString(quote, verseString) {
       const endIndex = verseString.indexOf(quoteChunk);
       missingWordsIndices.push(endIndex + (index === 0 ? quoteChunk.length : 0))
     } else {
-      console.log('3- else')
       if (!verseString.includes(quoteChunk)) {
         quoteChunk = quoteChunk.trim()
         quoteChunks[index] = quoteChunk
@@ -78,8 +74,6 @@ export function getQuoteOmittedString(quote, verseString) {
       missingWordsIndices.push(quoteChunkSubStrIndex + (index === 0 ? quoteChunk.length : 0))
     }
   })
-
-  console.log('missingWordsIndices', missingWordsIndices)
 
   const missingStrings = []
   missingWordsIndices.forEach((startIndex, i) => {
@@ -90,17 +84,11 @@ export function getQuoteOmittedString(quote, verseString) {
     }
   });
 
-  console.log('missingStrings', missingStrings)
-
   let wholeQuote = '';
   quoteChunks.forEach((chunk, index) => {
     const missingWord = missingStrings[index] || ''
     wholeQuote = wholeQuote + chunk + missingWord
   });
-
-  console.log('====================================');
-  console.log('wholeQuote', wholeQuote);
-  console.log('====================================');
 
   return wholeQuote;
 }
